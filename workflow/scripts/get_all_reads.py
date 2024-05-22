8849f3377db81701037bd8fba8a60c2a8ae584ce
Yuelong Chen <yuelong.chen.btr@gmail.com>
2024-05-22 11:17:12 +0800

    checking if the reads are existing

--- workflow/scripts/get_all_reads.py
+++ workflow/scripts/get_all_reads.py
@@ -20,19 +20,19 @@ def get_all_reads_from_fastq(fastq_file):
     print(f'{len(reads)} reads in {fastq_file}')
     return reads
 
+log = open(snakemake.log[0], "w")
+sys.stderr = log
+sys.stdout = log
 
+bam_reads = get_all_reads_from_bam(snakemake.input.bam)
+fastq_reads = get_all_reads_from_fastq(snakemake.input.fastq)
+blow5_reads = get_all_reads_from_blow5(snakemake.input.blow5)
 
-with open(snakemake.log[0], "w") as log:
-    sys.stderr = sys.stdout = log
-    bam_reads = get_all_reads_from_bam(snakemake.input.bam)
-    fastq_reads = get_all_reads_from_fastq(snakemake.input.fastq)
-    blow5_reads = get_all_reads_from_blow5(snakemake.input.blow5)
+all_reads = bam_reads.intersection(fastq_reads).intersection(blow5_reads)
 
-    all_reads = bam_reads.intersection(fastq_reads).intersection(blow5_reads)
+print(f'{len(all_reads)} reads in common between bam, fastq and blow5')
 
-    print(f'{len(all_reads)} reads in common between bam, fastq and blow5')
-
-    with open(snakemake.output.all_reads, 'w') as f:
-        for read in all_reads:
-            f.write(f'{read}\n')
+with open(snakemake.output.all_reads, 'w') as f:
+    for read in all_reads:
+        f.write(f'{read}\n')
 
